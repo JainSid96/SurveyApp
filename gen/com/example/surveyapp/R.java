8854282f49d24c3c5e7c9329bd64f94e2c55e01e
vasujain00 <www.vasujain@gmail.com>
2016-01-29 16:39:50 +0530

    Second Change
    Survey 2 page is updated

--- gen/com/example/surveyapp/R.java
+++ gen/com/example/surveyapp/R.java
@@ -25,13 +25,15 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
+        public static final int action_settings=0x7f08000e;
         public static final int button1=0x7f08000b;
+        public static final int button2=0x7f08000c;
         public static final int editText1=0x7f080006;
         public static final int editText2=0x7f080007;
         public static final int editText3=0x7f080004;
         public static final int editText4=0x7f080008;
         public static final int editText5=0x7f08000a;
+        public static final int ets2=0x7f08000d;
         public static final int textView1=0x7f080003;
         public static final int textView2=0x7f080000;
         public static final int textView3=0x7f080005;
@@ -43,11 +45,13 @@ public final class R {
         public static final int activity_main=0x7f030000;
         public static final int activity_survey1=0x7f030001;
         public static final int activity_survey2=0x7f030002;
+        public static final int activity_survey3=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
         public static final int survey1=0x7f070001;
         public static final int survey2=0x7f070002;
+        public static final int survey3=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -55,6 +59,7 @@ public final class R {
         public static final int hello_world=0x7f050002;
         public static final int title_activity_survey1=0x7f050003;
         public static final int title_activity_survey2=0x7f050004;
+        public static final int title_activity_survey3=0x7f050005;
     }
     public static final class style {
         /** 
--- src/com/example/surveyapp/Survey2.java
+++ src/com/example/surveyapp/Survey2.java
@@ -1,15 +1,30 @@
 package com.example.surveyapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Survey2 extends Activity {
-
+Button b1;
+Intent i;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_survey2);
+		b1=(Button)findViewById(R.id.button2);
+		b1.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				i=new Intent(Survey2.this,Survey3.class);
+				startActivity(i);
+			}
+		});
 	}
 
 	@Override
