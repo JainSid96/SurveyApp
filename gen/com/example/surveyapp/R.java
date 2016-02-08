78df76cd4e032fe37e709f3505b33c79c06d7538
vasujain00 <www.vasujain@gmail.com>
2016-02-08 20:31:37 +0530

    NewDrawableEditext

--- gen/com/example/surveyapp/R.java
+++ gen/com/example/surveyapp/R.java
@@ -22,10 +22,11 @@ public final class R {
     }
     public static final class drawable {
         public static final int bg=0x7f020000;
-        public static final int edittext=0x7f020001;
-        public static final int ic=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int street=0x7f020004;
+        public static final int edie=0x7f020001;
+        public static final int edittext=0x7f020002;
+        public static final int ic=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int street=0x7f020005;
     }
     public static final class id {
         public static final int RadioButton01=0x7f08001d;
@@ -39,7 +40,7 @@ public final class R {
         public static final int button1=0x7f080004;
         public static final int button2=0x7f080016;
         public static final int button25=0x7f080017;
-        public static final int button26=0x7f08001a;
+        public static final int button26=0x7f08001b;
         public static final int editText1=0x7f080009;
         public static final int editText2=0x7f08000a;
         public static final int editText3=0x7f080007;
@@ -56,7 +57,7 @@ public final class R {
         public static final int radioButton2=0x7f080019;
         public static final int ratingBar1=0x7f080023;
         public static final int ratingBar2=0x7f080024;
-        public static final int spinner1=0x7f08001b;
+        public static final int spinner1=0x7f08001a;
         public static final int spinner2=0x7f08001c;
         public static final int textView1=0x7f08000c;
         public static final int textView2=0x7f080003;
