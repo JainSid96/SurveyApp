8bf1bece7a897a268cbbacf9010e681582e670d0
vasujain00 <www.vasujain@gmail.com>
2016-03-11 14:29:19 +0530

    Survey3 intent done

--- src/com/example/surveyapp/Survey3.java
+++ src/com/example/surveyapp/Survey3.java
@@ -16,6 +16,7 @@ public class Survey3 extends Activity {
  Button b1,b2;
  GPSTracker gps;
  EditText et1,et2;
+ String lati,longi,tphone,mob,timein,timeout,other,id,name,loc,zip;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,6 +25,15 @@ public class Survey3 extends Activity {
 		b1 = (Button) findViewById(R.id.but1);
 		et1=(EditText)findViewById(R.id.editText8);
 		et2=(EditText)findViewById(R.id.editText9);
+		Bundle getUnique=getIntent().getExtras();
+	      id = getUnique.getString("id");
+		  name = getUnique.getString("name");
+		  loc = getUnique.getString("loc");
+	      zip = getUnique.getString("zip");
+	      tphone=getUnique.getString("tphone");
+	      mob=getUnique.getString("mob");
+	      timein=getUnique.getString("timein");
+	      timeout=getUnique.getString("timeout");
 		b2=(Button)findViewById(R.id.button2);
 		b1.setOnClickListener(new View.OnClickListener() {
 			
@@ -58,7 +68,23 @@ public class Survey3 extends Activity {
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
 				Intent i;
+				lati = et1.getText().toString();
+				longi = et2.getText().toString();
 				i=new Intent(Survey3.this,Survey4.class);
+				 Bundle ab = new Bundle();
+					
+				    ab.putString("id",id);
+				    ab.putString("name",name);
+				    ab.putString("loc",loc);
+				    ab.putString("zip",zip);
+				    ab.putString("tphone",tphone);
+				    ab.putString("mob",mob);
+				    ab.putString("other",other);
+				    ab.putString("timein",timein);
+				    ab.putString("timeout",timeout);
+				    ab.putString("lati", lati);
+				    ab.putString("longi",longi);
+				    i.putExtras(ab);
 				startActivity(i);
 				
 			}
