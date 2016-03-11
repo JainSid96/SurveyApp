5918007aebcf68e28d30ebf69ba0430c70d0e188
vasujain00 <www.vasujain@gmail.com>
2016-03-11 13:55:04 +0530

    Intents

--- src/com/example/surveyapp/Survey1.java
+++ src/com/example/surveyapp/Survey1.java
@@ -5,8 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.view.WindowManager;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -14,21 +14,22 @@ public class Survey1 extends Activity {
 Button b1;
 Intent i;
 EditText et1,et2,et3,et4;
-String a,b,c,d;
+String id,name,loc,zip;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_survey1);
 		b1=(Button)findViewById(R.id.button1);
-		/*et1=(EditText)findViewById(R.id.editText1);
+		et1=(EditText)findViewById(R.id.editText1);
 		et2=(EditText)findViewById(R.id.editText2);
 		et3=(EditText)findViewById(R.id.editText3);
-		et4=(EditText)findViewById(R.id.editText4);
-		a=et1.getText().toString();
-		b=et2.getText().toString();
-		c=et3.getText().toString();
-		d=et4.getText().toString(); */
+		et4=(EditText)findViewById(R.id.editText5);
+		
+		id=et1.getText().toString();
+		name=et2.getText().toString();
+		loc=et3.getText().toString();
+		zip=et4.getText().toString(); 
 		
 		b1.setOnClickListener(new OnClickListener() {
 			
@@ -36,6 +37,13 @@ String a,b,c,d;
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
 				i=new Intent(Survey1.this,Survey2.class);
+				Bundle ab = new Bundle();
+				
+			    ab.putString("id", id);
+			    ab.putString("name",name);
+			    ab.putString("loc",loc);
+			    ab.putString("zip",zip);
+			    i.putExtras(ab);
 				startActivity(i);
 			}
 		});
--- src/com/example/surveyapp/Survey2.java
+++ src/com/example/surveyapp/Survey2.java
@@ -13,28 +13,52 @@ import android.widget.EditText;
 public class Survey2 extends Activity {
 Button b1;
 Intent ii;
-EditText  et5,et6,et7,et8;
-String e,f,g,h;
+EditText  et5,et6,et7,et8,et9;
+String tphone,mob,timein,timeout,other,id,name,loc,zip;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_survey2);
 		b1=(Button)findViewById(R.id.b22);
-		/*et5=(EditText)findViewById(R.id.ets2);
-		et6=(EditText)findViewById(R.id.editText2);
-		et7=et5=(EditText)findViewById(R.id.editText3);
-		et8=(EditText)findViewById(R.id.editText4);
-		e=et5.getText().toString();
-		f=et6.getText().toString();
-		g=et7.getText().toString();
-		h=et8.getText().toString(); */
+	    et5=(EditText)findViewById(R.id.ets2); // tphone
+		et6=(EditText)findViewById(R.id.editText2); //mob
+		et7=et5=(EditText)findViewById(R.id.editText3); //other
+		et8=(EditText)findViewById(R.id.editText4); //timein
+		et9 =(EditText)findViewById(R.id.editText5); //timeout
+		
+		tphone=et5.getText().toString();
+		mob=et6.getText().toString();
+		other=et7.getText().toString();
+		timein=et8.getText().toString(); 
+		timeout = et9.getText().toString();
+		
+		Bundle getUnique=getIntent().getExtras();
+	      id = getUnique.getString("id");
+		 name = getUnique.getString("name");
+		 loc = getUnique.getString("loc");
+	      zip = getUnique.getString("zip");
 		b1.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
+				
 				ii=new Intent(Survey2.this,Survey3.class);
+				
+                Bundle ab = new Bundle();
+				
+			    ab.putString("id",id);
+			    ab.putString("name",name);
+			    ab.putString("loc",loc);
+			    ab.putString("zip",zip);
+			    ab.putString("tphone",tphone);
+			    ab.putString("mob",mob);
+			    ab.putString("other",other);
+			    ab.putString("timein",timein);
+			    ab.putString("timeout",timeout);
+			    
+			    ii.putExtras(ab);
 				startActivity(ii);
 			}
 		});
